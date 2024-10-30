2c01ef090e0fda0e9bd5be6d4acb64c032cc3b53
Leandro Varas <leovaras2005@gmail.com>
2024-10-30 17:01:21 +0100

    feat: •	Exemples d’entrada de dades i visualització amb 4 tipus de diàlegs.

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,124 @@ class _MyCustomFormState extends State<MyCustomForm> {
     super.dispose();
   }
 
+  void _showMenu() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Seleccione una opción'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                title: const Text('SimpleDialog'),
+                onTap: () {
+                  Navigator.of(context).pop(); // Cerrar el diálogo
+
+                  // Mostrar SimpleDialog con el título y texto escrito
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return SimpleDialog(
+                        title: const Text('SimpleDialog'),
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Text("      " + myController.text), // Texto ingresado
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+              ),
+
+              ListTile(
+                title: const Text('AlertDialog'),
+                onTap: () {
+                  Navigator.of(context).pop(); // Cerrar el diálogo
+
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text('AlertDialog'),
+                        content: Text(myController.text), // Muestra el texto ingresado
+                        actions: <Widget>[
+                          TextButton(
+                            child: const Text('OK'),
+                            onPressed: () {
+                              Navigator.of(context).pop(); // Cierra el AlertDialog
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+              ),
+              ListTile(
+                title: const Text('showSnackBar'),
+                onTap: () {
+                  Navigator.of(context).pop(); // Cerrar el diálogo
+
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: GestureDetector(
+                        onTap: () {
+                          ScaffoldMessenger.of(context).hideCurrentSnackBar(); // Oculta el SnackBar cuando se arrastra hacia abajo
+                        },
+                        child: Text(myController.text), // Muestra el texto ingresado
+                      ),
+                      backgroundColor: Colors.black, // Fondo negro
+                      behavior: SnackBarBehavior.fixed, // Se muestra en la parte inferior
+                    ),
+                  );
+                },
+              ),
+              ListTile(
+                title: const Text('showModalBottomSheet'),
+                onTap: () {
+                  Navigator.of(context).pop(); // Cierra el diálogo
+
+                  showModalBottomSheet( // Muestra la ventana emergente (BottomSheet)
+                    backgroundColor: Colors.transparent,
+                    context: context,
+                    builder: (BuildContext context) {
+                      return Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.amber,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(25),
+                            topRight: Radius.circular(25),
+                          ),
+                        ),
+                        height: 200,
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              Text(myController.text), // Muestra el texto ingresado
+                              ElevatedButton(
+                                onPressed: () => Navigator.of(context).pop(),
+                                child: const Text('Cerrar BottomSheet'),
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,38 +160,27 @@ class _MyCustomFormState extends State<MyCustomForm> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-              backgroundColor: Colors.transparent,
-              context: context,
-              builder: (BuildContext context) {
-                return Container(
-                    decoration: const BoxDecoration(
-                      color: Colors.amber,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(25),
-                        topRight: Radius.circular(25),
-                      ),
-                    ),
-                    height: 200,
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Text(myController.text),
-                          ElevatedButton(
-                            onPressed: () => Navigator.of(context).pop(),
-                            child: const Text('Tancar BottomSheet'),
-                          )
-                        ],
-                      ),
-                    ));
-              });
-        },
+        onPressed: _showMenu,
         tooltip: 'Mostra el valor!',
         child: const Icon(Icons.text_fields),
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
